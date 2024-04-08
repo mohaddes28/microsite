28a93d5f23d7e1d9187f85fcb1daf6840b15c5c3
mohaddes28 <mohaddes28@gmail.com>
2024-04-09 01:16:07 +0600

    new brunch correction

--- core/resources/views/frontend/blogs/includes/post-categories.blade.php
+++
@@ -1,5 +0,0 @@
-<ul class="widget-items">
-    @foreach($categories as $category)
-    <li><a href="{{ route('frontend/posts/category', ['slug' => $category->slug]) }}" class="d-flex p-3"><span>{{ $category->category_name }}</span><span class="ml-auto">({{ count($category->allPosts) }})</span></a></li>
-    @endforeach
-</ul>
--- core/resources/views/templates/no-data.blade.php
+++
@@ -1,1 +0,0 @@
-<p class="text-center"><img src="{{ asset('assets/etc/no-data-found.png') }}" alt="No Data Found"></p>
